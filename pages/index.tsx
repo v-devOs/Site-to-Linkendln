c81e66ff8ad6249b2e4babfb2c805cb6f8e1f0c4
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-07-20 01:23:43 -0600

    Borrando rutas inesesarias

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,85 +1,41 @@
-import NextLink from 'next/link'
-import Image from 'next/image'
-import { Avatar, Grid, IconButton, Typography, Link } from '@mui/material'
+import { AboutMe, Skills } from "@/components/main-page"
+import { skillsFrontEnd } from "@/data"
+import { useWrite } from "@/hooks"
+import { InitialPageLayout } from "@/layouts"
+import { Grid, Typography } from "@mui/material"
 
-import InstagramIcon from '@mui/icons-material/Instagram'
-import LinkedInIcon from '@mui/icons-material/LinkedIn'
-import WhatsAppIcon from '@mui/icons-material/WhatsApp'
-import ArrowOutwardOutlinedIcon from '@mui/icons-material/ArrowOutwardOutlined';
+const index2 = () => {
 
-import { InitialPageLayout } from '@/layouts'
-import { useWrite } from '@/hooks'
+  const { actualPhrase } = useWrite('Hola! Soy Uriel Galindo')
 
-
-const HomePage = () => {
-
-  const { actualPhrase } = useWrite('Uriel Galindo')
-  
   return (
-    <InitialPageLayout 
-      title='Uriel Emiliano Galindo Lopez' 
-      description='Sitio web creado para dar a conocer mis habilidades en desarrollo web y proyectos realizados'
-    >
-      <Grid container spacing={2} sx={{ width: { xs: '100%', md: '85%'}}}>
-
-        <Grid item>
-          <Typography variant='h1' component='h1' sx={{ fontFamily: 'serif'}}>{actualPhrase}</Typography>
-          <Typography variant='h2' mt={5}>Desarollador y diseñador web </Typography>
-        </Grid>
-
-
-          <Grid container display='flex' flexDirection='row'>
-
-            <Grid item mt={3} mr={5} mb={3}>
-              <Avatar alt='Uriel Galindo' src='/images/me.jpg' sx={{ width: 130, height: 130}}/>
-            </Grid>
-
-
-            <Grid item display='flex' flexDirection='column' mt={3} >
-              <IconButton>
-                <NextLink passHref href='https://instagram.com/uriel_egl?igshid=MzNlNGNkZWQ4Mg=='>
-                  <Link component='span'>
-                    <InstagramIcon/>
-                  </Link>
-                </NextLink>
-              </IconButton>
-
-              <IconButton>
-                <NextLink passHref href='https://wa.me/qr/QCHPQLGFDJH3D1'>
-                  <Link component='span'>
-                    <WhatsAppIcon/>
-                  </Link>
-                </NextLink>
-              </IconButton>
-
-              <IconButton>
-                <NextLink passHref href='https://www.linkedin.com/in/uriel-emiliano-galindo-l%C3%B3pez-27437a274'>
-                  <Link component='span'>
-                    <LinkedInIcon/>
-                  </Link>
-                </NextLink>
-              </IconButton>
-            </Grid>
-          </Grid>
-       
-
-          <Grid item>
-            <Typography variant='body1'>
-              Hola!, soy Uriel Emiliano Galindo lópez, un joven programador y desarrolador web apasionado con el desarrollo de apliaciones web 
-              personalizables y accesibles a las personas, paginas informativas, apliaciones web e-comerce
-            </Typography>
-          </Grid>
-
-          <Grid item>
-            <NextLink href='/contact-me' passHref>
-            <Link component='span'>
-              <ArrowOutwardOutlinedIcon/> Contactame
-            </Link>
-            </NextLink>
-          </Grid>
+    <InitialPageLayout title="Uriel Galindo" description="Mi sitio web creado para divulgar un poco de mis habilidades y proyectos realizados">
+      <Grid 
+        container 
+        className="container-header"
+      >
+        <header>
+          <Typography variant="h1" component='h1'>{ actualPhrase }</Typography>
+          <Typography variant="body2">'Si puedes imaginarlo puedes programarlo' - Alejandro Taboada</Typography>
+        </header>
       </Grid>
+
+      <Grid
+        container
+        className="ejemplo"
+        sx={{ m: { xs: '60vh 10px', md: '60vh 30px'}, background: '#101010', width: '100%', borderRadius: '20px', p: '20px'}}
+      >
+
+        <AboutMe/>
+
+        <Typography variant="h2" component='h2' mt={2}>Tecnologias para desarrollo</Typography>
+        <Skills skills={skillsFrontEnd} title="Front-End"/>
+
+      </Grid>
+
+
     </InitialPageLayout>
   )
 }
 
-export default HomePage
+export default index2
